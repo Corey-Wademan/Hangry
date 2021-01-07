72eef34cc54b3d36b98b3b4da6b9dacd42e282a8
Corey Wademan <67563215+Corey-Wademan@users.noreply.github.com>
2021-01-07 12:05:49 -0500

    rearranged file structure

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,18 +1,36 @@
 import React from 'react';
 import './App.css';
+import BusinessList from '../Business-List/BusinessList';
+import SearchBar from '../SearchBar/SearchBar';
 
-import BusinessList from '../Business/BusinessList.js'
-import SearchBar from '../SearchBar/SearchBar.js'
+import Yelp from '../../util/Yelp';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      businesses: []
+    };
+
+    
+  }
+
+  searchYelp(term, location, sortBy) {
+    Yelp.search(term, location, sortBy).then(businesses => {
+      this.setState({businesses: businesses});
+    });
+  }
+
   render() {
     return (
       <div className="App">
-      <h1>ravenous</h1>
-        <SearchBar />
-        <BusinessList />
-    </div> 
+        <h1>ravenous</h1>
+        <SearchBar searchYelp={this.searchYelp.bind(this)} />
+        <BusinessList businesses={this.state.businesses} />
+      </div>
     );
   }
-};
+}
+
 export default App;
