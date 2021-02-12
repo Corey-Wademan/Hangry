20a8e775b8cd7d4888c7708ceab0c615650403c0
Corey Wademan <67563215+Corey-Wademan@users.noreply.github.com>
2021-02-12 10:04:26 -0500

    Updated Headers

--- src/util/Yelp.js
+++ src/util/Yelp.js
@@ -1,10 +1,11 @@
 const apiKey = `${process.env.apiKey}`
 const Yelp = {
     search(term, location, sortBy) {
-      return fetch(`https://thingproxy.freeboard.io/fetch/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`, 
+      return fetch(`https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`, 
       {
         headers: {
-          Authorization: `Bearer ${apiKey}`
+          Authorization: `Bearer ${apiKey}`,
+          Access-Control-Access-Origin: '*'
         }
       }).then(response => {
         return response.json();
