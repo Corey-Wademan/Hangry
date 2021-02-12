d4000652afd3d1d5d9344358bc7effc01a8a5e94
Corey Wademan <67563215+Corey-Wademan@users.noreply.github.com>
2021-02-11 23:07:45 -0500

    Update Yelp.js

--- src/util/Yelp.js
+++ src/util/Yelp.js
@@ -1,4 +1,4 @@
-
+const apiKey = `${process.env.apiKey}`
 const Yelp = {
     search(term, location, sortBy) {
       return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`, 
